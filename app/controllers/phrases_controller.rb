2e668cb4d25ab523e4471c5fb5cd925433aa6cb7
jazzari <passionjazz@gmail.com>
2020-06-16 17:17:53 +0200

    Finish CRUD actions for phrases controller with TDD

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -12,25 +12,43 @@ class PhrasesController < ApplicationController
 		@phrase = Phrase.new
 	end
 
+	def edit
+	end
+
 	def create
 		@phrase = Phrase.new(phrase_params)
 
 		if @phrase.save
-			redirect_to root_path, notice: 'Faphra was successfully created'
+			flash[:success] = "Faphra was successfully created"
+			redirect_to root_path
 		else
 			render :new
 		end
 	end
 
+	def update
+		@phrase = Phrase.find(params[:id])
+		if @phrase.update(phrase_params)
+			flash[:success] = "Faphra was updated successfully!"
+			redirect_to phrase_path(@phrase)
+		else
+			render 'edit'
+		end 
+	end
 
+	def destroy
+		@phrase.destroy
+		flash[:notice] = "Faphra was deleted successfully"
 
+		redirect_to phrases_path
+	end
 
 
 
 	private
 
 	  def phrase_params
-	  	params.require(:phrase).permit(:body)
+	  	params.require(:phrase).permit(:id, :body)
 	  end
 
 end
--- spec/controllers/phrases_controller_spec.rb
+++ spec/controllers/phrases_controller_spec.rb
@@ -48,12 +48,91 @@ describe PhrasesController do
 
 	describe 'POST phrase#create' do 
 
-		it "should create a new phrase" do 
+		context "with valid attributes" do 
+
+			it "should save the phrase in the database" do 
+				expect{
+	       		post :create, params: { :phrase => { :body => 'My favorite phrase' }
+	            } }.to change(Phrase, :count).by 1
+				
+				expect(flash[:success]).to eq("Faphra was successfully created")
+			end
+
+			it "should redirect to root path after creating" do 
+				post :create, params: { :phrase => { :body => 'My favorite phrase' } }
+				expect(response).to redirect_to root_path
+			end
+
+		end
+
+		context "with invalid attributes" do 
+
+			it "should not save the phrase in the database" do 
+				expect{
+		       		post :create, params: { :phrase => { :body => '' }
+		            } }.to_not change(Phrase, :count)
+			end
+
+			it "should redirect to new phrase template after failing create" do 
+				post :create, params: { :phrase => { :body => '' } }
+				expect(response).to render_template('new')
+			end
+
+		end
+
+	end
+
+	describe 'PATCH phrases#update' do 
+		let(:phrase) { create :phrase }
+		subject { get :show, params: { id: phrase.id } }
+
+		context "with valid attributes" do
+
+			it "should update an existing phrase" do 
+				patch :update, :params => { id: phrase.id, 
+					:phrase => { :body => "My updated favorite phrase" } }
+				expect(flash[:success]).to eq("Faphra was updated successfully!") 
+			end
+
+			it "should redirect to show template after updating" do 
+				patch :update, :params => { id: phrase.id, 
+					:phrase => { :body => "My updated favorite phrase" } }
+				expect(response).to redirect_to phrase_path(phrase)
+			end
+
+		end
+
+		context "with invalid attributes" do 
+
+			it "should redirect to edit template after failing update" do 
+				patch :update, :params => { id: phrase.id, 
+					:phrase => { :body => "" } }
+
+				expect(response).to render_template('edit')
+			end
+
+		end
+
+	end
+
+	describe 'DELETE phrases#destroy' do 
+		let(:phrase) { create :phrase }
+		subject { get :show, params: { id: phrase.id } }
+
+		it "should destroy current faphra in database" do 
+			subject
+
 			expect{
-       		post :create, params: { :phrase => { :body => 'My favorite phrase' }
-            } }.to change(Phrase, :count).by 1
-			
-			expect(flash[:notice]).to eq("Faphra was successfully created")
+	       		delete :destroy, params: { :id => phrase.id }
+	             }.to change(Phrase, :count).by(-1)
+			expect(flash[:notice]).to eq("Faphra was deleted successfully")
+		end
+
+		it "should redirect to index after delete faphra" do 
+			subject
+
+	       	delete :destroy, params: { :id => phrase.id }
+	        expect(response).to redirect_to phrases_path
 		end
 
 	end
