a8aa1a4c3130672687ea05530455f26ab609213b
jazzari <passionjazz@gmail.com>
2020-06-30 11:00:50 +0200

    Adding search form to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def search
+		if params[:search].blank?  
+    		redirect_to(root_path, alert: "Empty field!") and return  
+  		else  
+  			@parameter = params[:search].downcase
+  			@results = User.all.where('lower(username) LIKE :search', 
+  				search: "%#{@parameter}%")
+  		end 
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post ':id/unfollow_user', to: 'friendships#unfollow_user', as: :unfollow_user
  
 
+  get 'users/search', to: 'users#search', as: 'search_page'
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users, only: [:show]
   root to: 'phrases#index'
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
   end
 
   get '/ownphrases', to: 'users#index'
+
 
   
   get 'friendships/following', to: 'friendships#following'
